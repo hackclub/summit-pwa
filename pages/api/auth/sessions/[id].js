ac33077a390a4ad06c1c29df21fa3394da3afcc6
Sam Poder <sam.r.poder@gmail.com>
2024-01-30 00:52:09 -0800

    special special route

---
+++ pages/api/auth/sessions/[id].js
@@ -0,0 +1,20 @@
+import { findAttendee } from "@/lib/airtable";
+import sendgrid, { loginCodeEmail }  from "@/lib/email";
+import { createTempSession, generateLoginCode } from "@/lib/sessions";
+import { setCookie } from "cookies-next";
+
+const oneWeek = 60 * 60 * 24 * 7;
+
+export default async function handler(req, res) {
+  const attendee = await findAttendeeById(req.query.id);
+  if (!attendee) return res.json({ registered: false });
+
+  const { sessionId } = await create(attendee.fields.email, true);
+  setCookie("session", sessionId, { req, res, maxAge: oneWeek });
+
+  const { loginCode } = await generateLoginCode(sessionId);
+  
+  await loginWithCode(sessionId, loginCode);
+
+  return res.redirect("/");
+}
