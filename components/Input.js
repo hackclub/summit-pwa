7c3bbf31c58c0589cb217d01effb3568c62cd6ce
Yoda <yoda@yodacode.xyz>
2024-02-02 10:00:07 -0800

    input

--- components/Input.js
+++ components/Input.js
@@ -1,20 +1,39 @@
+import { useId } from "react"
+import { Akaya_Kanadaka } from "next/font/google";
+
+const akaya = Akaya_Kanadaka({ weight: "400", subsets: ["latin"] });
+
 export default function Input ({ label, name, value, onChange, placeholder }) {
+  const id = useId();
+
   return (
     <div style={{
       display: "flex",
+      flexDirection: "column",
       gap: "16px",
-      height: "32px",
-      alignItems: "center",
-      maxWidth: "400px"
+      alignItems: "start",
+      maxWidth: "400px",
+      justifyContent: "start"
     }}>
-      <label for={name}>{label}</label>
+      <label for={name} style={{
+        fontSize: "24px",
+      }}>{label}</label>
+      <style jsx>{`
+        #${name}::placeholder {
+          color: var(--red);
+          opacity: 0.7;
+        }
+
+      `}</style>
       <input name={name} id={name} placeholder={placeholder} type="text" style={{
         width: "100%",
         height: "32px",
-        borderRadius: "4px",
         border: "1px solid var(--red)",
-        padding: "0 8px",
-      }} value={value} onChange={onChange} />
+        background: "var(--tan)",
+        color: "var(--red)",
+        fontSize: "20px",
+        padding: "12px 12px",
+      }} value={value} className={akaya.className} onChange={onChange} />
     </div>
   )
 }
