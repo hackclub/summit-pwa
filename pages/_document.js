0dfa7c12803e0bf52a6816cff9effdb23635f043
Yoda <yoda@yodacode.xyz>
2024-02-03 01:05:01 -0800

    Theme bar

--- pages/_document.js
+++ pages/_document.js
@@ -11,6 +11,7 @@ export default function Document() {
         <link rel="apple-touch-startup-image" href="/assets/apple.png" />
         <meta name="apple-mobile-web-app-title" content="Summit" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       </Head>
       <body style={{ margin: "0px!important" }}>
         <Main />
