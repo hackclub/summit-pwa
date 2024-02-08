5ebdfa4d39863183b77c113ec3f9b8c1c26359f4
Sam Poder <sam.r.poder@gmail.com>
2024-02-08 15:41:19 -0800

    handle edge case

--- pages/api/attendee/generateTicket.js
+++ pages/api/attendee/generateTicket.js
@@ -2,13 +2,15 @@ import { updateAttendeeById } from "@/lib/airtable";
 import Session from "@/lib/sessions";
 import { ticketNumber } from "@/utils/ticketNumber";
 
-export default async function generateTicket (req, res) {
+export default async function handler (req, res) {
   const session = await Session.from(req, res);
   const user = await session.currentAuthorizedUser();
-
-  const number = await ticketNumber(user.fields.first_name + " " + user.fields.last_name, user.fields.email);
-
-  await updateAttendeeById(user.id, {"ticketing_ticketNumber": +number});
-
+  await generateTicket(user)
   res.json({ success: true });
+}
+
+export async function generateTicket(user){
+  const number = await ticketNumber(user.fields.first_name + " " + user.fields.last_name, user.fields.email);
+  await updateAttendeeById(user.id, {"ticketing_ticketNumber": +number});
+  return true
 }
